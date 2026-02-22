d582efb687574e975d636e63ee71c2dab77116d0
usacoincollector <usacoincollector.ebay@gmail.com>
2026-02-22 02:55:18 +0000

    Fix Vercel build: papaparse types and Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -9,7 +9,7 @@ export function getStripeClient() {
 
   if (!stripeClient) {
     stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2024-12-18.acacia'
+      apiVersion: '2025-02-24.acacia'
     });
   }
 
